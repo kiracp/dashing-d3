24031da1cfd5714bae1aaab7d2bdef5ec539fa6e
kcp288 <kira.prentice@nyu.edu>
2018-02-15 19:43:55 -0500

    setting up week 4

---
+++ week 4/kp-pace-contd/README.md
@@ -0,0 +1,5 @@
+# Continuing the pace viz
+
+The first thing I did this week was clean up the dataset to include dates!
+
+My csv export last week didn't have dates. To get that info, I literally copied and pasted plain text from the Garmin site and edited that ugly data into a pretty csv. See the `pace.txt` for the original file, the `format-csv.py` for a command line python script I ran to output `pretty-pace.csv` -- a workable csv with all the info I want.
---
+++ week 4/kp-pace-contd/format-csv.py
@@ -0,0 +1,43 @@
+def main():
+	# Ugly pace file with info on newlines
+	input = open('pace.txt', 'r').readlines()
+
+	#Output file
+	output = open('pretty-pace.txt', 'w')
+
+	count = 0
+	
+	# This will contain our csv row
+	row = ""
+
+	#Each input item is 15 lines
+	#Our job is to break those lines up and determine which number contains which value
+	for i in input:
+		i = i.strip()
+
+		# Day-month
+		if (count%15 == 1): 
+			row = ""
+			i = i.split()
+			row = i[0]+"-"+i[1]
+		#Year
+		if (count%15 == 2): 
+			row = row + "-" + i
+		#Dist
+		if (count%15 == 5): 
+			i = i.split()
+			row = row + "," + i[0]
+		#Time
+		if (count%15 == 7): 
+			row = row + "," + i
+		#Pace
+		if (count%15 == 9): 
+			i = i.split()
+
+			row = row + "," + i[0] + "\n"
+			#print row
+			output.write(row)
+
+		count=count+1
+	
+main()
